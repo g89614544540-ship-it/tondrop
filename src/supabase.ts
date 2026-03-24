7f1959d27eac1ca8c8cc91ff907315e17555cfa2
TonDrop <test@test.com>
2026-03-24 05:28:04 +0300

    fix supabase url

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  'https://fkdnuaiiqmgbhkpstucq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrZG51YWlpcW1nYmhrcHN0dWNxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjczNjIzOCwiZXhwIjoyMDU4MzEyMjM4fQ.j6n77CZGBKO3wPwMBBWqYkQEMcMH3DnStk3YLmyRY1Q'
+  'https://idpzpzxjwtlzljcsqkil.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkcHpwenhqd3RsemxqY3Nxa2lsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDI4Nzk4MSwiZXhwIjoyMDg5ODYzOTgxfQ.SqCzBod47lKZPvadzUBhYQ5SF_Ywd8ouIpzvGwINx4c'
 );
 
 export default supabase;
